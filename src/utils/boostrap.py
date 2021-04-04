5f421466a387af2950221b3c512841ed875a9b41
Jonathan Martins <jonathansilvamartinscanada@gmail.com>
2021-04-04 19:54:35 -0300

    Add a few comments for clarity

--- src/utils/boostrap.py
+++ src/utils/boostrap.py
@@ -1,10 +1,17 @@
 import random
 
 class Bootstrap():
-
+  """
+  underlying_sample: list of test instances from where the bag items will be drawn
+  """
   def __init__(self, underlying_sample):
     self.underlying_sample = underlying_sample
 
+  """
+  Creates a new bag using sampling with replacement
+  @input: the size of the list of instances that is returned by the method
+  @output: a list of size length filled with test instances randomly chosen from the underlying sample
+  """
   def get_new_bag(self, length):
     bag = []
     for _ in range(length):
